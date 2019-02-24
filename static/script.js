70e0759321a3c12110ad614689ce3ffa0fc1b0d4
Kalle Lindqvist <kalle.lindqvist@mykolab.com>
2019-02-24 16:27:39 +0100

    Handle API errors and document all Javascript code

--- static/script.js
+++ static/script.js
@@ -1,49 +1,78 @@
 // Declare globals to satisfy standardJS
-/* global XMLHttpRequest FormData alert $ */
+/* global XMLHttpRequest FormData $ */
 
+/**
+ * This code runs after the html code has finished loading.
+ */
 window.addEventListener('load', () => {
-  const processData = (JSONdata) => {
-    var data = JSON.parse(JSONdata)
-    var table = $('#analysisTable')
-    var tableCaption = $('#analysisTableCaption')
-
-    // Clear the table
-    table.find('tbody tr').remove()
-
-    if (data.data) {
-      data.data.forEach((row) => {
-        table.append('<tr><td>' + (row.headline ? row.headline : 'Ingen') + '</td><td>' + row.message + '</td></tr>')
-      })
-    }
-    tableCaption.text(data.message)
-  }
-
-  const sendData = () => {
-    var XHR = new XMLHttpRequest()
-
-    var formData = new FormData()
-    formData.append('file', document.getElementById('docxSelector').files[0])
-
-    // Define what happens on successful data submission
-    XHR.addEventListener('load', (event) => {
-      alert(event.target.responseText)
-      processData(event.target.responseText)
-    })
-
-    // Define what happens in case of error
-    XHR.addEventListener('error', (event) => {
-      alert('Oj! Något gick åt skogen')
-    })
-
-    XHR.open('POST', 'http://127.0.0.1:5000/api/docx')
-    XHR.send(formData)
-  }
-
+  // take over the forms submit event.
   var form = document.getElementById('docxForm')
-
-  // take over the forms its submit event.
-  form.addEventListener('submit', function (event) {
+  form.addEventListener('submit', (event) => {
     event.preventDefault()
     sendData()
   })
 })
+
+/**
+ * Create and dispatches a bootstrap alert signaling danger/error.
+ */
+const sendAlert = (message) => {
+  var alert = $('<div class="alert alert-danger" role="alert">' +
+                '<button type="button" class="close" data-dismiss="alert" ' +
+                'aria-label="Close"><span aria-hidden="true">&times;</span>' +
+                '</button>' + message + '</div>')
+  $('.container').prepend(alert)
+  window.setTimeout(() => {
+    $('.alert').fadeTo(500, 0).slideUp(500, () => {
+      $(this).remove()
+    })
+  }, 4000)
+}
+
+/**
+ * Process the API response.
+ */
+const processData = (JSONdata) => {
+  var data = JSON.parse(JSONdata)
+
+  if (data.code !== 200) {
+    sendAlert(data.message)
+    return
+  }
+
+  var table = $('#analysisTable')
+  var tableCaption = $('#analysisTableCaption')
+
+  // Clear the table
+  table.find('tbody tr').remove()
+
+  if (data.data) {
+    data.data.forEach((row) => {
+      table.append('<tr><td>' + (row.headline ? row.headline : 'Ingen') + '</td><td>' + row.message + '</td></tr>')
+    })
+  }
+  tableCaption.text(data.message)
+}
+
+/**
+ * Sends the .docx file to the API for processing.
+ */
+const sendData = () => {
+  var XHR = new XMLHttpRequest()
+
+  var formData = new FormData()
+  formData.append('file', document.getElementById('docxSelector').files[0])
+
+  // Define what happens on successful data submission
+  XHR.addEventListener('load', (event) => {
+    processData(event.target.responseText)
+  })
+
+  // Define what happens in case of error
+  XHR.addEventListener('error', (event) => {
+    sendAlert('Oj! Något gick åt skogen.')
+  })
+
+  XHR.open('POST', 'http://127.0.0.1:5000/api/docx')
+  XHR.send(formData)
+}
