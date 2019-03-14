11b3a70173adb5eebd0e2644f4597e2c09eade9f
Kalle Lindqvist <kalle.lindqvist@mykolab.com>
2019-03-14 11:07:18 +0100

    add sentence class

---
+++ src/report/sentence.py
@@ -0,0 +1,20 @@
+import xml.etree.ElementTree as ET
+
+from src.report.word import Word
+
+
+class Sentence:
+    """Represents a sentence from a report."""
+
+    def __init__(self, sentence_node: ET.Element) -> None:
+        self.words = []
+        self.text = sentence_node.attrib["original"]
+
+        for word_node in sentence_node:
+            if word_node.tag == "w":
+                self.words.append(Word(word_node))
+            elif word_node.tag == "ne":  # named entity
+                for word_ne in word_node:
+                    self.words.append(Word(word_ne))
+            else:
+                raise Exception(f'Unreqognized tag {word_node.tag}')
